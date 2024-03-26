6526d2eb3a8ea99517175231727ac3f41b8101fe
Xuanbach <xuanbach14052002@gmail.com>
2024-03-26 20:58:05 +0700

    feat

--- sentiment.py
+++ sentiment.py
@@ -1,18 +1,13 @@
 import re
 import pickle
-import pandas as pd
 import joblib
+import pandas as pd
 
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 from nltk import download
 download('stopwords')
 download('wordnet')
-
-vectoriser = joblib.load('tfidf_vectoriser.pkl')
-
-with open('Sentiment-LightGBM.pickle', 'rb') as file:
-    LGBMmodel = pickle.load(file)
 
 emojis = {':)': 'smile', ':-)': 'smile', ';d': 'wink', ':-E': 'vampire', ':(': 'sad', 
           ':-(': 'sad', ':-<': 'sad', ':P': 'raspberry', ':O': 'surprised',
@@ -72,92 +67,26 @@ def preprocess(textdata):
         processedText.append(tweetwords)
     return processedText
 
-def predict(vectoriser, model, text):
-    textdata = vectoriser.transform(preprocess(text)) 
-    sentiment = model.predict(textdata)
-    
+def load_model():
+    vectoriser = joblib.load('tfidf_vectoriser.pkl')
+    with open('Sentiment-LightGBM.pickle', 'rb') as file:
+        LGBMmodel = pickle.load(file)
+    return vectoriser, LGBMmodel
+
+vectoriser, LGBMmodel = load_model()
+
+def preprocess_text(text):
+    processed_text = preprocess(text)
+    return processed_text
+
+def predict(text):
+    processed_text = preprocess_text(text)
+    textdata = vectoriser.transform(processed_text)
+    sentiment = LGBMmodel.predict(textdata)
     data = []
     for text, pred in zip(text, sentiment):
         data.append((text,pred))
-        
+    # 0 is Negative, 1 is Positive
     df = pd.DataFrame(data, columns = ['text','sentiment'])
-    df = df.replace([0,1], ["Negative","Positive"])
+
     return df
-
-# if __name__=="__main__": 
-#     # Text to classify should be in a list.
-#     text = ["Today is so great!",
-#             "May the Good Lord be with you.", "I hate peanuts!",
-#             "Mr. Kehinde, what are you doing next? this is great!"]
-    
-#     df = predict(vectoriser, LGBMmodel, text)
-#     print(df.head())
-
-# from kafka import KafkaConsumer
-# import json
-# consumer = KafkaConsumer('test', bootstrap_servers=['leesin.click:9092'])
-
-# # Đọc dữ liệu từ Kafka và gọi hàm predict
-# for message in consumer:
-    
-#     data = json.loads(message.value)
-#     text_to_predict = data['tweet'] 
-    
-#     # Gọi hàm predict để phân loại sentiment cho tin nhắn
-#     result_df = predict(vectoriser, LGBMmodel, [text_to_predict])
-    
-#     # Xử lý kết quả ở đây (ví dụ: lưu vào cơ sở dữ liệu, gửi đi qua Kafka producer, in ra màn hình, v.v.)
-#     print(result_df.head())
-
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
-import time
-from configparser import ConfigParser
-
-conf_file_path = "/home/luongdb123/realtime_data_processing/"
-conf_file_name = conf_file_path + "stream_app.conf"
-config_obj = ConfigParser()
-config_read_obj = config_obj.read(conf_file_name)
-
-# Khởi tạo SparkSession
-spark = SparkSession.builder \
-    .appName("SentimentAnalysis") \
-    .getOrCreate()
-
-# Định nghĩa schema cho dữ liệu JSON
-schema = StructType() \
-    .add("created_at", TimestampType()) \
-    .add("tweet_id", StringType()) \
-    .add("tweet", StringType())
-
-# Đọc dữ liệu từ Kafka vào DataFrame
-kafka_df = spark \
-    .readStream \
-    .format("kafka") \
-    .option("kafka.bootstrap.servers", "leesin.click:9092") \
-    .option("subscribe", "test") \
-    .load()
-  
-# Parse dữ liệu JSON từ Kafka
-parsed_df = kafka_df \
-    .selectExpr("CAST(value AS STRING)") \
-    .select(from_json("value", schema).alias("data")) \
-    .select("data.*")
-
-predict_udf = udf(lambda tweet: predict(vectoriser, LGBMmodel, [tweet]), StringType())
-
-# Gọi hàm predict để phân loại sentiment cho mỗi tin nhắn
-result_df = parsed_df \
-    .select("tweet") \
-    .withColumn("sentiment", predict_udf("tweet"))
-
-# In kết quả ra màn hình
-query = result_df \
-    .writeStream \
-    .outputMode("append") \
-    .format("console") \
-    .start()
-
-# Chờ cho query kết thúc
-query.awaitTermination()
